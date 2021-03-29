6a3b56fae6d98bcb0e45ccbbd3a5413e421847c0
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-29 16:28:12 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,18 @@
+let x = 1;
+let y = 2;
+
+let res1 = '12';
+console.log(res1); 
+console.log(typeof res1);
+
+let res2 = 'true2';
+console.log(res2); 
+console.log(typeof res2); 
+
+let res3 = true;
+console.log(res3); 
+console.log(typeof res3); 
+
+let res4 = 'one' * 2;
+console.log(res4);
+console.log(typeof res4);
