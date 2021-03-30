317d00a617b29aa6f0777dd62d3194b37b904842
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-30 22:21:23 +0300

    Create task5.js

---
+++ task5.js
@@ -0,0 +1,8 @@
+let x = prompt('Are you adult?'); 
+if ( x > 0 && x < 17){
+  console.log('Ви ще надто молоді')
+  }else if(x > 17){
+  console.log('Ви досягли повнолітнього віку')
+  }else{
+  console.log('Помилка')
+  } 
