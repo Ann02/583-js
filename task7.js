44e46f395c0c0b278adcfdd22955104ec93f90b1
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-30 23:31:17 +0300

    Create task7.js

---
+++ task7.js
@@ -0,0 +1,11 @@
+let date = new Date();
+let hours = date.getHours();
+if (hours >= 5 && hours < 11){
+  alert ('Доброго ранку')
+}else if(hours >= 11 && hours < 17){
+  alert ('Доброго дня')
+}else if(hours >= 17 && hours < 23){
+  alert ('Доброго ранку')
+}else if(hours >= 23 && hours < 24 || hours >= 0 && hours < 5 ){
+  alert ('Доброго ранку')
+}
