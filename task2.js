da6d8cb9146841b71691ee1660236a2db61f332a
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-30 21:18:33 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,9 @@
+let x = prompt('Enter the number '); 
+if ( x > 0 && (x % 2) == 0 ){
+  console.log('Число є парним додатним')}
+
+let x = prompt('Enter the number '); 
+if(x % 7){
+  console.log('Число не є кратним 7')
+}else{
+  console.log('Число є кратним 7')}
