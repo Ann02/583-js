90457959bd8f301563634222e1488e9c72138560
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-30 22:58:26 +0300

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,6 @@
+let x = prompt('Enter the length of the first side ');  x = Number;
+let y = prompt('Enter the length of the second side '); y = Number;
+let z = prompt('Enter the length of the third side '); z = Number
+let p = (x + y + z)/ 2; 
+let S = (p(p-x)(p-y)(p-z))*0,5; 
+console.log(S);
