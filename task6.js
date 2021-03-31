c1d00d5db603811c8ed61445260273c5f05652a0
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-03-31 22:33:27 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,6 +1,9 @@
-let x = prompt('Enter the length of the first side ');  x = Number;
-let y = prompt('Enter the length of the second side '); y = Number;
-let z = prompt('Enter the length of the third side '); z = Number
+let x = prompt('Enter the length of the first side ');  x = Number(x);
+let y = prompt('Enter the length of the second side '); y = Number(y);
+let z = prompt('Enter the length of the third side '); z = Number(z);
 let p = (x + y + z)/ 2; 
-let S = (p(p-x)(p-y)(p-z))*0,5; 
-console.log(S);
+if ( z ** 2 = (x ** 2) + (y ** 2) ){
+  console.log('Цей трикутник є прямокутним');
+}
+let s = (p(p-x)(p-y)(p-z))*0,5;
+console.log(s);
